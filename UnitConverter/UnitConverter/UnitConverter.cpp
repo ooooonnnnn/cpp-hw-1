b34701c986a4df85f1db703c8d96241c59118a13
ooooonnnnn <79533443+ooooonnnnn@users.noreply.github.com>
2026-04-19 22:44:32 +0300

    refactored unit choosing into function

--- UnitConverter/UnitConverter/UnitConverter.cpp
+++ UnitConverter/UnitConverter/UnitConverter.cpp
@@ -8,70 +8,25 @@
 
 using namespace std;
 
-const int num_units = 4;
-string units[num_units] = {"m", "km", "cm", "feet"};
-float values_in_meters[num_units] = {1, 1000, 0.01f, 0.3048f};
+vector<string> units = {"m", "km", "cm", "feet"};
+vector<double> values_in_meters = {1, 1000, 0.01f, 0.3048f};
+
+double input_choose_unit(string prompt, string* unit_name);
 
 void main()
 {
-    cout << "Welcome to the unit converter!\n\n";
+    //Greet user
+    cout << "\nWelcome to the unit converter!\n\n";
     
     // choose base unit from list
-    bool is_valid = false;
-    int base_unit_ind = -1;
-    while(!is_valid)
-    {
-        cout << "What is your base unit? { ";
-        for (auto unit : units)
-        {
-            cout << unit << " ";
-        }
-        cout << "}\n";
-        
-        string chosen_unit;
-        cin >> chosen_unit;
-        for(int i = 0; i < num_units; i++)
-        {
-            if (chosen_unit == units[i])
-            {
-                base_unit_ind = i;
-                is_valid = true;
-            }
-        }
-        if (is_valid) break;
-        cout << "No such unit.\n";
-    }
+    string base_unit_name;
+    double base_unit_value = input_choose_unit("What is your base unit?", &base_unit_name);
     
-    // choose target unit from sub list
-    is_valid = false;
-    int target_unit_ind = -1;
-    while(!is_valid)
-    {
-        cout << "What is your target unit? { ";
-        string unit;
-        for (int i = 0; i < num_units; i++)
-        {
-            if (i == base_unit_ind) continue;
-            unit = units[i];
-            cout << unit << " ";
-        }
-        cout << "}\n";
-        
-        string chosen_unit;
-        cin >> chosen_unit;
-        for(int i = 0; i < num_units; i++)
-        {
-            if (i == base_unit_ind) continue;
-            
-            if (chosen_unit == units[i])
-            {
-                target_unit_ind = i;
-                is_valid = true;
-            }
-        }
-        if (is_valid) break;
-        cout << "No such unit.\n";
-    }
+    // choose target unit from remaining list
+    string target_unit_name;
+    double target_unit_value = input_choose_unit("What is your target unit?", &target_unit_name);
+    
+    double trans_factor = base_unit_value / target_unit_value;
     
     //enter numbers
     while (true)
@@ -91,10 +46,43 @@ void main()
             return;
         }
         
-        cout << parsed_number << units[base_unit_ind] << " = " << 
-            parsed_number * values_in_meters[base_unit_ind] / values_in_meters[target_unit_ind] << units[target_unit_ind] <<
+        cout << parsed_number << " " << base_unit_name << " = " << 
+            parsed_number * trans_factor << " " << target_unit_name <<
                 "\n";
         
         cout << "Enter a non number to exit\n";
+    }
+}
+
+///Prompts the user to type the name of a unit from the list, then deletes that unit.
+///@param unit_name The name of the chosen unit
+///@return The value in meters of the chosen unit
+double input_choose_unit(string prompt, string* unit_name = nullptr)
+{
+    double unit_value;
+    while(true)
+    {
+        cout << prompt << " { ";
+        for (auto unit : units)
+        {
+            cout << unit << " ";
+        }
+        cout << "}\n";
+        
+        string chosen_unit;
+        cin >> chosen_unit;
+        for(int i = 0; i < units.size(); i++)
+        {
+            if (chosen_unit == units[i])
+            {
+                unit_value = values_in_meters[i];
+                *unit_name = units[i];
+                
+                units.erase(units.begin() + i);
+                values_in_meters.erase(values_in_meters.begin() + i);
+                return unit_value;
+            }
+        }
+        cout << "No such unit.\n";
     }
 }
